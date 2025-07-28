038ec503dda9e6be5f17356ccce8810b606c3e23
Krish-sankhala <krishsankhala8@gmail.com>
2025-07-28 11:17:57 +0530

    Add files via upload

---
+++ assignment/17 July (Movie Explorer)/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UpcomingMovies from './pages/UpcomingMovies';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movies/upcoming" element={<UpcomingMovies />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
